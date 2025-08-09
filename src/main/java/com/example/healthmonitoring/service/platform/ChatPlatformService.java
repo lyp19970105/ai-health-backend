ca2ec63630122cb47ec27b59d92ab5f7b58d099a
梁有佩 <1178907791@qq.com>
2025-08-09 15:09:03 +0800

    refactor(conversation): 代码合并

--- src/main/java/com/example/healthmonitoring/service/platform/ChatPlatformService.java
+++
@@ -1,9 +0,0 @@
-package com.example.healthmonitoring.service.platform;
-
-import com.example.healthmonitoring.dto.frontend.ChatRequest;
-import com.example.healthmonitoring.model.LlmAppConfig;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-public interface ChatPlatformService {
-    void streamChatResponse(ChatRequest frontendRequest, LlmAppConfig appConfig, SseEmitter emitter);
-}
